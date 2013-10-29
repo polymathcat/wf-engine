b8360e4c998dc489c27ae7af317e71ffa513d258
Ruben Acuna <racuna1@asu.edu>
2013-10-28 21:17:08 -0700

    Updating to defaults generated by Leiningen 2.3.3.

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
 (defproject wf-engine "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
-  :aot [wf-engine.core]
-  :main wf-engine.core)
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                  [lacij "0.8.1"]]
+  :main ^:skip-aot wf-engine.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
+
