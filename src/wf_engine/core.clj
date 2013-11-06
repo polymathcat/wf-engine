217f0079d79b7f1a2f8fbeabc35409feeb4c967e
polymathcat <racuna1@asu.edu>
2013-11-05 17:21:31 -0700

    Copied in the dynamic graph example to check build environment.

--- src/wf_engine/core.clj
+++ src/wf_engine/core.clj
@@ -3,12 +3,15 @@
   (:use
 
        lacij.model.graph
-       lacij.edit.graph
+       lacij.edit.dynamic      ;not sure if is right
        lacij.view.graphview
        lacij.layouts.layout
-   ;     (lacij.layouts core layout)
-   )
-  )
+       (tikkba swing dom core)
+       tikkba.utils.xml)
+
+  (:import (javax.swing JFrame JButton BoxLayout SwingUtilities)
+           (java.awt.event ActionListener)
+           java.awt.Component))
 
 ;next three are from https://github.com/pallix/lacij/tree/master/src/lacij/layouts
 ;(defn add-nodes [g & nodes]
@@ -215,7 +218,7 @@
 [])
 
 
-(design-make-task :delta-delta-g-prediction sprouts-ontology)
+;(design-make-task :delta-delta-g-prediction sprouts-ontology)
 
 
 (design-make-task :interaction-prediction sprouts-ontology)
@@ -230,6 +233,83 @@
   []
   (println "Hello, World!"))
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;; copied from swing utils
+(defn add-action-listener
+  "Adds an ActionLister to component. When the action fires, f will be
+invoked with the event as its first argument followed by args.
+Returns the listener."
+  [component f & args]
+  (let [listener (proxy [ActionListener] []
+                   (actionPerformed [event] (apply f event args)))]
+    (.addActionListener component listener)
+    listener))
+
+(defn on-action [event svgcanvas g]
+  (do-batik
+   svgcanvas
+   (-> g
+       (add-node! :appolon "Appolon" :x 50 :y 350)
+       (add-edge! :appolon-athena :appolon :athena))))
+
+(defn gen-graph
+  []
+  (-> (graph)
+      (add-node :athena "Athena" :x 10 :y 30)
+      (add-node :zeus "Zeus" :x 200 :y 150)
+      (add-node :hera "Hera" :x 500 :y 150)
+      (add-node :ares "Ares" :x 350 :y 250)
+      (add-node :matrimony "<3" :x 400 :y 170 :shape :circle)
+      (add-edge :father1 :athena :zeus)
+      (add-edge :zeus-matrimony :zeus :matrimony)
+      (add-edge :hera-matrimony :hera :matrimony)
+      (add-edge :son-zeus-hera :ares :matrimony)
+      (build)))
+
+(defn create-frame
+  [svgcanvas g]
+  (let [frame (JFrame.)
+        button (JButton. "Action")
+        pane (.getContentPane frame)]
+    (add-action-listener button on-action svgcanvas g)
+    (.setLayout pane (BoxLayout. pane BoxLayout/Y_AXIS))
+    (.setAlignmentX button Component/CENTER_ALIGNMENT)
+    (.add pane button)
+    (.add pane svgcanvas)
+    (.setSize frame 800 600)
+    (.setSize svgcanvas 800 600)
+    frame))
+
+(defn zz []
+  (let [g (gen-graph)
+        doc (:xmldoc g)
+        _ (export g "dynamic1.svg")
+        svgcanvas (:svgcanvas g)
+        frame (create-frame svgcanvas g)]
+    (SwingUtilities/invokeAndWait
+     (fn [] (.setVisible frame true)))))
+
+;(zz)
 
 
 
