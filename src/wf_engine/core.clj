46a62f736291faf5ade2d16496db47a1a2cf88c9
Ruben Acuna <racuna1@asu.edu>
2013-11-12 23:21:23 -0700

    Split ontology code into own file.

--- src/wf_engine/core.clj
+++ src/wf_engine/core.clj
@@ -2,6 +2,7 @@
   (:gen-class)
   (:use
        wf-engine.database
+       wf-engine.ontology
        lacij.model.graph
        lacij.edit.graph
        lacij.edit.dynamic      ;not sure if is right
@@ -90,69 +91,6 @@
             (build))))
 
 (export sprouts-ontology "simple.svg" :indent "yes")
-
-(:nodes (second sprouts-ontology))
-(first (:nodes (second sprouts-ontology)))
-(:id (first (:nodes (second sprouts-ontology))))
-(first (first (:nodes (second sprouts-ontology))))
- (:text (first (:labels (:view (second (first (:nodes (second sprouts-ontology))))))))
-
-;;;;ONTOLOGY
-
-;ONLY IDs should be exposed to the outside world
-;hmm... question mark expects everything, other functions assume is okay?.
-
-;private
-
-(defn ontology-get-node-byid [id ontology]
-  (second (first (filter #(= (first %) id)
-                         (:nodes (second ontology))))))
-
-(defn ontology-get-edge-byid [id ontology]
-  (second (first (filter #(= (first %) id)
-                         (:edges (second ontology))))))
-
-;(:outedges (ontology-get-node-byid :interaction-prediction sprouts-ontology))
-;(:text (first (:labels (:view (ontology-get-edge-byid :predicts1 sprouts-ontology)))))
-;(count (:outedges (ontology-get-node-byid :interaction-prediction sprouts-ontology)))
-;(:text (first (:labels (:view (ontology-get-edge-byid :uses1 sprouts-ontology)))))
-
-(defn ontology-get-edge-type [edge ontology]
-  (:text (first (:labels (:view edge)))))
-
-(ontology-get-edge-type (ontology-get-edge-byid :uses1 sprouts-ontology) sprouts-ontology)
-
-
-;public
-(defn ontology? [ontology]
-  (and (vector? ontology)
-       (= (first ontology) :ontology)))
-
-(defn ontology-node-data-centric? [id ontology]
-  (if (ontology? ontology)
-    (let [node (ontology-get-node-byid id ontology)]
-      ;all out edges are labeled predict or uses and one of each exists
-      (let [count-predicts (count (filter #(= (ontology-get-edge-type (ontology-get-edge-byid % ontology)
-                                                                      ontology)
-                                              "predicts")
-                                          (:outedges node)))
-
-                count-uses (count (filter #(= (ontology-get-edge-type (ontology-get-edge-byid % ontology)
-                                                                      ontology)
-                                              "predicts")
-                                          (:outedges node)))]
-            (and (> count-predicts 0)
-                 (> count-uses 0)
-                 (= (count (:outedges node)) (+ count-predicts count-uses)))))
-    false))
-
-
-;(ontology-node-data-centric? :mutation sprouts-ontology)
-;(ontology-node-data-centric? :interaction-prediction sprouts-ontology)
-
-
-(defn ontology-node-name [id ontology]
-  (:text (first (:labels (:view (ontology-get-node-byid id ontology))))))
 
 
 ;;;;PROCOTOLS
@@ -318,6 +256,8 @@ Returns the listener."
      (fn [] (.setVisible frame true)))))
 
 (zz)
+
+
 
 
 
---
+++ src/wf_engine/ontology.clj
@@ -0,0 +1,58 @@
+;;; Copyright ©2013 Ruben Acuna
+
+;;; namespace and externals
+
+(ns wf-engine.ontology
+  (:gen-class))
+
+;;; procedures
+
+;ONLY IDs should be exposed to the outside world
+;hmm... question mark expects everything, other functions assume is okay.
+
+;private
+
+(defn ontology-get-node-byid [id ontology]
+  (second (first (filter #(= (first %) id)
+                         (:nodes (second ontology))))))
+
+(defn ontology-get-edge-byid [id ontology]
+  (second (first (filter #(= (first %) id)
+                         (:edges (second ontology))))))
+
+(defn ontology-get-edge-type [edge ontology]
+  (:text (first (:labels (:view edge)))))
+
+;(ontology-get-edge-type (ontology-get-edge-byid :uses1 sprouts-ontology) sprouts-ontology)
+
+
+;public
+(defn ontology? [ontology]
+  (and (vector? ontology)
+       (= (first ontology) :ontology)))
+
+(defn ontology-node-data-centric? [id ontology]
+  (if (ontology? ontology)
+    (let [node (ontology-get-node-byid id ontology)]
+      ;all out edges are labeled predict or uses and one of each exists
+      (let [count-predicts (count (filter #(= (ontology-get-edge-type (ontology-get-edge-byid % ontology)
+                                                                      ontology)
+                                              "predicts")
+                                          (:outedges node)))
+
+                count-uses (count (filter #(= (ontology-get-edge-type (ontology-get-edge-byid % ontology)
+                                                                      ontology)
+                                              "predicts")
+                                          (:outedges node)))]
+            (and (> count-predicts 0)
+                 (> count-uses 0)
+                 (= (count (:outedges node)) (+ count-predicts count-uses)))))
+    false))
+
+
+;(ontology-node-data-centric? :mutation sprouts-ontology)
+;(ontology-node-data-centric? :interaction-prediction sprouts-ontology)
+
+
+(defn ontology-node-name [id ontology]
+  (:text (first (:labels (:view (ontology-get-node-byid id ontology))))))
